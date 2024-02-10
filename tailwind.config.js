707c4d256cc34434e2eacd120569d792df52680c
Suryansh777777 <suryanshchourasia12@gmail.com>
2024-02-11 04:30:17 +0530

    card added

--- tailwind.config.js
+++ tailwind.config.js
@@ -75,6 +75,9 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      compilerOptions: {
+        jsx: "react",
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
